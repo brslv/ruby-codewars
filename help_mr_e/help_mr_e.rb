625f7874f4f7b10d38df4edcc7496384d0e6f446
Borislav Grigorov <brslvg@gmail.com>
2016-12-01 07:30:36 +0200

    Add [help_mr_e].

---
+++ help_mr_e/help_mr_e.rb
@@ -0,0 +1,13 @@
+=begin
+Mr. E Ven only likes even length words. Please create a translator so that he doesn't have to hear those pesky odd length words. For some reason he also hates punctuation, he likes his sentences to flow.
+
+Your translator should take in a string and output it with all odd length words having an extra letter (the last letter in the word). It should also remove all punctuation (.,?!) as well as any underscores (_).
+
+"How did we end up here? We go?" translated becomes-> "Howw didd we endd up here We go"
+=end
+
+def evenator(s)
+	s.tr('.,?!_', '').split.map{ |w| w.chars.length.even? ? w : w += w[-1] }.join(' ')
+end
+
+p evenator("odd odd odd o odd ev eve even")
